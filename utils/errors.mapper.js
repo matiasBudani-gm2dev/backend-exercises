984e2df753991b7ae5d0563bde2c5580efa01cbf
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-10-20 12:22:56 -0300

    fix: check for error message in mapError

--- utils/errors.mapper.js
+++ utils/errors.mapper.js
@@ -1,7 +1,7 @@
 import { createError } from "/Users/gm2academy/Documents/GitHub/backend-exercises/utils/createError.js"
 
 export function mapError(err){
-    if(err.statusCode && err.type){
+    if(err.statusCode && err.type && err.message){
         return err
     }
     return createError(500, "Internal Server Error", "Ha ocurrido un error inesperado")
