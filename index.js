92a5ab68ae42ca45451b0407dc4d49d5ebb3869d
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-05 13:07:23 -0300

    fix: remove unnecessary console log statement in server listening logic

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import {createApp} from './app.js';
 const PORT = process.env.PORT || 3000;
 
 createApp().listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
-console.log(`🎯 Arquitectura: Routes → Service → Repository + Model → MySQL DB`)
+  console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
