693f056a2e44543b272846cb794ce543dcbdeff1
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-06 11:00:26 -0300

    fix: include MYSQL_PORT in Sequelize configuration

--- boostrap.js
+++ boostrap.js
@@ -4,11 +4,15 @@ dotenv.config()
 
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, 
-    process.env.MYSQL_PASSWORD, {
-  host: process.env.MYSQL_HOST,
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE, 
+  process.env.MYSQL_USER, 
+  process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql'
+  }
+);
 
 
 
