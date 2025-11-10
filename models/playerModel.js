c72f4b184af8547d2a7760f70159872bcd91bb38
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-10 10:26:41 -0300

    feat: add Players model with player_id, createdAt, nick, position, and number fields

---
+++ models/playerModel.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import {sequelize} from "../boostrap.js";
+
+const Players = sequelize.define("Players", {
+  player_id: {
+    type: DataTypes.INTEGER, 
+    primaryKey: true, 
+    autoIncrement: true,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: sequelize.literal("CURRENT_TIMESTAMP")
+  },
+  nick : {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  position: {
+    type: DataTypes.ENUM('forward', 'midfielder', 'defender', 'goalkeeper'),
+    allowNull: false
+  },
+  number : {
+    type: DataTypes.NUMBER
+  },
+  position: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+})
