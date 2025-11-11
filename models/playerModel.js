66f29dc12e63acb501cc8d3767ca7f2c73752044
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-11 12:50:44 -0300

    fix: change literal default value fro created_at to DATE.NOW

--- models/playerModel.js
+++ models/playerModel.js
@@ -1,35 +1,41 @@
 import { DataTypes } from "sequelize";
-import {sequelize} from "../boostrap.js";
+import { sequelize } from "../boostrap.js";
 
-const Players = sequelize.define("Players", {
-  player_id: {
-    type: DataTypes.INTEGER, 
-    primaryKey: true, 
-    autoIncrement: true,
-    allowNull: false
+const Players = sequelize.define(
+  "Players",
+  {
+    player_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    nick: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    position: {
+      type: DataTypes.ENUM("forward", "midfielder", "defender", "goalkeeper"),
+      allowNull: false,
+    },
+    number: {
+      type: DataTypes.INTEGER,
+    },
   },
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: sequelize.literal("CURRENT_TIMESTAMP")
+  {
+    tableName: "players",
+    timestamps: false,
   },
-  nick : {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  position: {
-    type: DataTypes.ENUM('forward', 'midfielder', 'defender', 'goalkeeper'),
-    allowNull: false
-  },
-  number : {
-    type: DataTypes.NUMBER
-  },
-  position: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-}, {
-  tableName: "players",
-  timestamps: false
-})
+);
+
+export default Players;
