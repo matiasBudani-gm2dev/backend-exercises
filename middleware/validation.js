06dd5d6657dc8046dd7d4a3e964ab6ee0c5406e0
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-11 11:44:06 -0300

    feat: structure using reduce error messages

--- middleware/validation.js
+++ middleware/validation.js
@@ -7,15 +7,16 @@ export function schemaReqValidation(schema) {
       stripUnknown: true,
     });
     if (error) {
-      let errorMessages = {};
-      error.details.map((detailErr) => {
-        logger.info(detailErr);
-        const field =
-          detailErr.context.label || detailErr.path?.[0] || "unknown";
-        errorMessages[field] = detailErr.message;
-      });
-      res.status(400).send(errorMessages);
-      console.log(errorMessages);
+      const invalid_fields = error.details.reduce(
+        (accum, { context, path, message }) => {
+          const field = context.label || path[0] || "unknown";
+          return { ...accum, [field]: message };
+        },
+        {},
+      );
+
+      console.log(invalid_fields);
+      res.status(400).send({ invalid_fields });
       return;
     } else {
       next();
