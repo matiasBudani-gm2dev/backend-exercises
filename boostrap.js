4bc1e4b1099b692ce01c066c78efa89b82759361
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-11 12:14:36 -0300

    feat: move to product configuration

--- boostrap.js
+++ boostrap.js
@@ -7,10 +7,10 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
-  process.env.PASSWORD,
+  process.env.MYSQL_PASSWORD,
   {
-    host: process.env.HOST,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: "mysql",
   },
 );
