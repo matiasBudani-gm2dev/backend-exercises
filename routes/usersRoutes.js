f6a32cf4fb289be48de69f121cacf6f19cc94a98
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-11 12:52:43 -0300

    fix: remove duplicated delete route

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -54,71 +54,75 @@ userRouter.get(
     } catch (error) {
       next(error);
     }
-})
+  },
+);
 
-userRouter.get('/:id', authenticateToken, authorizeRoles(["user", "admin"]), async (req, res, next) => {
-    try{
-        const id = (Number(req.params.id))
-        const user = await getUserById(id)
-        res.status(200).send(user)
-    }catch(error){
-        next(error)
+userRouter.get(
+  "/:id",
+  authenticateToken,
+  authorizeRoles(["user", "admin"]),
+  async (req, res, next) => {
+    try {
+      const id = Number(req.params.id);
+      const user = await getUserById(id);
+      res.status(200).send(user);
+    } catch (error) {
+      next(error);
     }
-})
+  },
+);
 
-userRouter.post('/',
-    schemaReqValidation(createUserSchema),
-    authenticateToken, authorizeRoles(["admin"]),
-        async (req, res, next) => {     
-        try{
-            const {user_name, email, password} = req.body
-            const passwordHash = await bcrypt.hash(password, 10)
-            const user = await createNewUser({user_name, email, passwordHash })
+userRouter.post(
+  "/",
+  schemaReqValidation(createUserSchema),
+  authenticateToken,
+  authorizeRoles(["admin"]),
+  async (req, res, next) => {
+    try {
+      const { user_name, email, password } = req.body;
+      const passwordHash = await bcrypt.hash(password, 10);
+      const user = await createNewUser({ user_name, email, passwordHash });
 
-            res.status(201).send(user)
-        }catch(error){
-            next(error)
-        }
+      res.status(201).send(user);
+    } catch (error) {
+      next(error);
     }
-)
+  },
+);
 
+userRouter.put(
+  "/:id",
+  schemaReqValidation(updateCompleteUserSchema),
+  authenticateToken,
+  authorizeRoles(["admin"]),
+  async (req, res, next) => {
+    try {
+      const id = Number(req.params.id);
+      const { user_name, email } = req.body;
 
-userRouter.put('/:id', 
-    schemaReqValidation(updateCompleteUserSchema), 
-    authenticateToken, authorizeRoles(["admin"]),
-    async (req, res, next) => {
-    try{
-        const id = Number(req.params.id)
-        const {user_name, email } = req.body;
-
-        const user = await updateUserComplete(id, {user_name, email })    
-        res.status(200).send(user)    
-    }catch(error){
-        next(error)
+      const user = await updateUserComplete(id, { user_name, email });
+      res.status(200).send(user);
+    } catch (error) {
+      next(error);
     }
-})
+  },
+);
 
+userRouter.patch(
+  "/:id",
+  schemaReqValidation(updatePartialUserSchema),
+  authenticateToken,
+  authorizeRoles(["admin"]),
+  async (req, res, next) => {
+    try {
+      const id = Number(req.params.id);
+      const { user_name, email } = req.body;
 
-userRouter.patch('/:id', schemaReqValidation(updatePartialUserSchema), 
-authenticateToken, authorizeRoles(["admin"]),
-async (req, res, next) => {
-    try{   
-        const id = Number(req.params.id)
-        const {user_name, email } = req.body;
+      const user = await updateUserPartial(id, { user_name, email });
 
-        const user = await updateUserPartial(id, {user_name, email})
-   
-        res.status(200).send(user)   
-    }catch(error){
-        next(error)
-    }
-})
-
-userRouter.delete('/:id',authenticateToken, authorizeRoles(["admin"]), async (req, res, next) => { 
-    try{ 
-        const id = Number(req.params.id)
-        const user = await deleteUser(id)
-        res.status(200).send(user)
+      res.status(200).send(user);
+    } catch (error) {
+      next(error);
     }
   },
 );
