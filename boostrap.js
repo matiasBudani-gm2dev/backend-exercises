842e5a3bfc001a23eaa9c2bbaebe159ff89db35c
matiasBudani-gm2dev <matias.budani@gm2dev.com>
2025-11-12 12:07:01 -0300

    feat: add deleted status to player instead of delete it from database

--- boostrap.js
+++ boostrap.js
@@ -4,13 +4,26 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
+// SERVER
+// const sequelize = new Sequelize(
+//   process.env.MYSQL_DATABASE,
+//   process.env.MYSQL_USER,
+//   process.env.MYSQL_PASSWORD,
+//   {
+//     host: process.env.MYSQL_HOST,
+//     port: process.env.MYSQL_PORT,
+//     dialect: "mysql",
+//   },
+// );
+
+// LOCAL
 const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USER,
-  process.env.MYSQL_PASSWORD,
+  process.env.PASSWORD,
   {
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
 );
